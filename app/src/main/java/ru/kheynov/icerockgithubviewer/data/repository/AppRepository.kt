d5469c911bed7c0f2576c78b2c9975857678c422
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-13 12:10:46 +0700

    feat: implement AppRepository

--- app/src/main/java/ru/kheynov/icerockgithubviewer/data/repository/AppRepository.kt
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/data/repository/AppRepository.kt
@@ -1,5 +1,9 @@
 package ru.kheynov.icerockgithubviewer.data.repository
 
+import android.util.Base64
+import okio.ByteString.Companion.decodeBase64
+import ru.kheynov.icerockgithubviewer.data.api.GithubApi
+import ru.kheynov.icerockgithubviewer.data.api.KeyValueStorage
 import ru.kheynov.icerockgithubviewer.data.entities.Repo
 import ru.kheynov.icerockgithubviewer.data.entities.RepoDetails
 import ru.kheynov.icerockgithubviewer.data.entities.UserInfo
@@ -7,14 +11,23 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AppRepository @Inject constructor() {
-
+class AppRepository @Inject constructor(
+    private val githubApi: GithubApi,
+    private val keyValueStorage: KeyValueStorage,
+) {
     suspend fun getRepositories(): List<Repo> {
-        TODO()
+        return githubApi.getRepositoriesList(
+            authHeader = "Bearer ${keyValueStorage.authToken}",
+            userName = keyValueStorage.userName!!
+        )
     }
 
     suspend fun getRepository(repoId: String): RepoDetails {
-        TODO()
+        return githubApi.getRepositoryDetails(
+            ownerName = keyValueStorage.userName!!,
+            repoName = repoId,
+            authHeader = "Bearer ${keyValueStorage.authToken}",
+        )
     }
 
     suspend fun getRepositoryReadme(
@@ -22,10 +35,19 @@ class AppRepository @Inject constructor() {
         repositoryName: String,
         branchName: String,
     ): String {
-        TODO()
+        val res = githubApi.getRepositoryReadme(
+            ownerName,
+            repositoryName,
+            branchName,
+            authHeader = "Bearer ${keyValueStorage.authToken}"
+        )
+        return Base64.decode(res.content.toByteArray(), Base64.DEFAULT).decodeToString()
     }
 
     suspend fun signIn(token: String): UserInfo {
-        TODO()
+        keyValueStorage.authToken = token
+        val res = githubApi.getUserInfo(authHeader = "Bearer ${keyValueStorage.authToken}")
+        keyValueStorage.userName = res.login
+        return res
     }
 }
