d5c73eb0f7d2babaaeba24421a5fad1f7381f613
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-13 10:08:45 +0700

    chore: add di module for retrofit

---
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/di/AppModule.kt
@@ -0,0 +1,39 @@
+package ru.kheynov.icerockgithubviewer.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import ru.kheynov.icerockgithubviewer.data.api.GithubApi
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideBaseURL(): String = "https://api.github.com/"
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideRetrofit(BASE_URL: String): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(retrofit: Retrofit): GithubApi = retrofit.create(GithubApi::class.java)
+}
