11acd5dbbefbac895b8fc8dd0f255b2bb82de27a
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-15 20:07:29 +0700

    feat: add colorful language labels for repositories list items

--- app/src/main/java/ru/kheynov/icerockgithubviewer/data/entities/Repo.kt
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/data/entities/Repo.kt
@@ -1,6 +1,8 @@
 package ru.kheynov.icerockgithubviewer.data.entities
 
+import android.graphics.Color
 import kotlinx.serialization.Serializable
+import ru.kheynov.icerockgithubviewer.data.language_colors.LanguageColors
 
 @Serializable
 data class Repo(
@@ -8,5 +10,7 @@ data class Repo(
     val id: Int,
     val owner: UserInfo,
     val description: String?,
-    val language: String
-)
+    val language: String?,
+) {
+    fun getColor(): Int = LanguageColors.colorsOfLanguages[language] ?: Color.WHITE
+}
