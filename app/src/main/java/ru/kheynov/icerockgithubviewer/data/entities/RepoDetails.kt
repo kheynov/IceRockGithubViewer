25aee5dc4504c718c129c0621244ea3f6d82fcc5
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-16 21:34:02 +0700

    feat: add license object to repo model

--- app/src/main/java/ru/kheynov/icerockgithubviewer/data/entities/RepoDetails.kt
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/data/entities/RepoDetails.kt
@@ -6,8 +6,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class RepoDetails(
     val name: String,
+    val owner: UserInfo,
+    @SerialName("default_branch") val defaultBranch: String,
     @SerialName("forks_count") val forksCount: Int,
     @SerialName("watchers_count") val watchersCount: Int,
     @SerialName("stargazers_count") val starsCount: Int,
-    val license: String?,
+    val license: License?,
+    val description: String?,
+    @SerialName("html_url") val url: String,
+)
+
+@Serializable
+data class License(
+    val name: String
 )
