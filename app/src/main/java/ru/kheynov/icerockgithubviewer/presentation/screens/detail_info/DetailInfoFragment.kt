70a019008c0e4bbf3d4c3f164e89f2ad28bc06c3
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-16 21:33:47 +0700

    feat: created detail info screen view model and domain logic

--- app/src/main/java/ru/kheynov/icerockgithubviewer/presentation/screens/detail_info/DetailInfoFragment.kt
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/presentation/screens/detail_info/DetailInfoFragment.kt
@@ -1,28 +1,42 @@
 package ru.kheynov.icerockgithubviewer.presentation.screens.detail_info
 
-import android.content.res.ColorStateList
-import android.graphics.Color
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
-import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
-import io.noties.markwon.core.MarkwonTheme
+import ru.kheynov.icerockgithubviewer.BuildConfig
 import ru.kheynov.icerockgithubviewer.R
 import ru.kheynov.icerockgithubviewer.databinding.FragmentDetailInfoBinding
+import ru.kheynov.icerockgithubviewer.presentation.screens.detail_info.DetailInfoViewModel.ReadmeState
+import ru.kheynov.icerockgithubviewer.presentation.screens.detail_info.DetailInfoViewModel.State.Loaded
+import ru.kheynov.icerockgithubviewer.presentation.screens.detail_info.DetailInfoViewModel.State.Loading
+import ru.kheynov.icerockgithubviewer.utils.RepositoryError
+
+private const val TAG = "DetailInfo"
 
 @AndroidEntryPoint
 class DetailInfoFragment : Fragment() {
 
     private lateinit var navController: NavController
+
     private lateinit var binding: FragmentDetailInfoBinding
+
+    private val viewModel by viewModels<DetailInfoViewModel>()
+
+    private val repoName: String
+        get() = requireNotNull(requireArguments().getString(REPOSITORY_NAME_KEY))
+
+    private lateinit var markDownRenderer: Markwon
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,15 +49,134 @@ class DetailInfoFragment : Fragment() {
             false
         )
 
-        binding.apply {
-            forksCount.text = "30"
-            starsCount.text = "3"
-            watchersCount.text = "10"
-            license.text = "Apache-2.0"
-            link.text = "github.com/icerockdev/moko-resources"
+        if (viewModel.state.value !is Loaded) viewModel.fetchRepository(repoName)
+
+        binding.detailInfoErrorButton.setOnClickListener {
+            if (viewModel.state.value is Loaded) {
+                (viewModel.state.value as Loaded).githubRepo.let {
+                    viewModel.fetchReadme(
+                        it.name,
+                        it.defaultBranch,
+                        it.owner.login
+                    )
+                }
+            } else {
+                viewModel.fetchRepository(repoName)
+            }
         }
 
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            if (BuildConfig.DEBUG) Log.i(TAG, state.toString())
+            binding.apply {
+                repositoryDetailInfoBlock.visibility =
+                    if (state is Loaded) View.VISIBLE else View
+                        .INVISIBLE
+                forksCount.text =
+                    if (state is Loaded) state.githubRepo.forksCount.toString() else ""
+                starsCount.text =
+                    if (state is Loaded) state.githubRepo.starsCount.toString() else ""
+                watchersCount.text =
+                    if (state is Loaded) state.githubRepo.watchersCount.toString() else ""
+                license.text =
+                    if (state is Loaded)
+                        state.githubRepo.license?.name ?: "No license"
+                    else ""
+                link.text =
+                    if (state is Loaded) state.githubRepo.url.substringAfter("//") else ""
+
+                link.setOnClickListener {
+                    if (state is Loaded) openUrl(state.githubRepo.url)
+                }
+
+                detailInfoLoadingPb.visibility = if (state is Loading) View.VISIBLE else View
+                    .INVISIBLE
+
+                bindErrorScreen(
+                    visibility = state is DetailInfoViewModel.State.Error,
+                    error = if (state is DetailInfoViewModel.State.Error) state.error else null,
+                )
+
+            }
+        }
+
+        viewModel.readmeState.observe(viewLifecycleOwner) { state ->
+            Log.i(TAG, state.toString())
+            binding.apply {
+                repositoryDetailReadmeBlock.visibility =
+                    if (state !is ReadmeState.Loading) View.VISIBLE else View.INVISIBLE
+
+                if (state is ReadmeState.Empty) {
+                    readmeTextView.text = "No README.MD"
+                }
+
+                if (state is ReadmeState.Loaded) {
+                    markDownRenderer.setMarkdown(readmeTextView,
+                        state.markdownToString())
+                }
+                readmeLoadingPb.visibility = if (state is ReadmeState.Loading) View.VISIBLE else
+                    View.INVISIBLE
+
+                bindErrorScreen(
+                    visibility = state is ReadmeState.Error,
+                    error = if (state is ReadmeState.Error) state.error else null,
+                )
+            }
+
+        }
         return binding.root
+    }
+
+    private fun FragmentDetailInfoBinding.bindErrorScreen(
+        // binding error view
+        visibility: Boolean = false,
+        error: RepositoryError?,
+    ) {
+        detailInfoErrorButton.apply {
+            setOnClickListener {
+                viewModel.fetchRepository(repoName)
+            }
+            text = when (error) {
+                is RepositoryError.NetworkError -> getString(R.string.retry_button_label)
+                else -> getString(R.string.refresh_button_label)
+            }
+        }
+
+        detailInfoErrorScreen.visibility = if (visibility)
+            View.VISIBLE
+        else
+            View.INVISIBLE
+
+
+        detailInfoErrorTitle.apply {
+            text = when (error) {
+                is RepositoryError.NetworkError -> getString(R.string.connection_error)
+                else -> getString(R.string.error)
+            }
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.error))
+        }
+
+        detailInfoErrorDescription.text = when (error) {
+            is RepositoryError.NetworkError -> {
+                getString(R.string.check_your_internet_connection)
+            }
+            else -> {
+                getString(
+                    R.string.error_repositories,
+                    if (error is RepositoryError.Error) error.message else "",
+                )
+            }
+        }
+
+        detailInfoErrorImage.setImageDrawable(
+            context?.let {
+                when (error) {
+                    is RepositoryError.NetworkError -> ContextCompat.getDrawable(it,
+                        R.drawable.ic_no_connection)
+                    is RepositoryError.Error -> ContextCompat.getDrawable(it, R.drawable.ic_error)
+                    else -> ContextCompat.getDrawable(it, R.drawable.ic_error)
+                }
+            }
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,6 +191,7 @@ class DetailInfoFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_action_logout -> {
+                viewModel.logOut()
                 navController.navigate(R.id.action_detailFragment_to_authFragment)
                 true
             }
@@ -74,58 +208,24 @@ class DetailInfoFragment : Fragment() {
         navController = Navigation.findNavController(view)
 
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
-            title = "TODO"
+            title = repoName
             setHomeAsUpIndicator(R.drawable.ic_arrow_left)
             setDisplayHomeAsUpEnabled(true)
             show()
         }
+        markDownRenderer = Markwon.create(requireContext())
+    }
 
-        val markwon = Markwon.builder(context!!)
-            .usePlugin(object : AbstractMarkwonPlugin() {
-                override fun configureTheme(builder: MarkwonTheme.Builder) {
-                    builder.codeTextColor(Color.GREEN)
-                }
-            }).build()
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
 
+    companion object {
+        private const val REPOSITORY_NAME_KEY = "repoNameKey"
 
-        val markdown = "# H1\n" +
-                "## H2\n" +
-                "### H3\n" +
-                "#### H4\n" +
-                "##### H5\n" +
-                "###### H6\n" +
-                "\n" +
-                "Alternatively, for H1 and H2, an underline-ish style:\n" +
-                "\n" +
-                "Alt-H1\n" +
-                "======\n" +
-                "\n" +
-                "Alt-H2\n" +
-                "## H2\n" +
-                "### H3\n" +
-                "#### H4\n" +
-                "##### H5\n" +
-                "###### H6\n" +
-                "\n" +
-                "Alternatively, for H1 and H2, an underline-ish style:\n" +
-                "\n" +
-                "Alt-H1\n" +
-                "======\n" +
-                "\n" +
-                "Alt-H2\n" +
-                "## H2\n" +
-                "### H3\n" +
-                "#### H4\n" +
-                "##### H5\n" +
-                "###### H6\n" +
-                "\n" +
-                "Alternatively, for H1 and H2, an underline-ish style:\n" +
-                "\n" +
-                "Alt-H1\n" +
-                "======\n" +
-                "\n" +
-                "Alt-H2\n" +
-                "------"
-        markwon.setMarkdown(binding.readmeTextView, markdown)
+        fun createArguments(repoName: String): Bundle {
+            return bundleOf(REPOSITORY_NAME_KEY to repoName)
+        }
     }
 }
---
+++ app/src/main/java/ru/kheynov/icerockgithubviewer/presentation/screens/detail_info/DetailInfoViewModel.kt
@@ -0,0 +1,132 @@
+package ru.kheynov.icerockgithubviewer.presentation.screens.detail_info
+
+import android.util.Base64
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.*
+import ru.kheynov.icerockgithubviewer.BuildConfig
+import ru.kheynov.icerockgithubviewer.data.entities.RepoDetails
+import ru.kheynov.icerockgithubviewer.data.repository.AppRepository
+import ru.kheynov.icerockgithubviewer.utils.RepositoryError
+import javax.inject.Inject
+
+private const val TAG = "DetailInfoVM"
+
+@HiltViewModel
+class DetailInfoViewModel @Inject constructor(
+    private val repository: AppRepository,
+) : ViewModel() {
+
+    private var fetchRepositoryDetailsJob: Job? = null
+    private var fetchRepositoryReadmeJob: Job? = null
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+    private val _readmeState = MutableLiveData<ReadmeState>()
+    val readmeState: LiveData<ReadmeState>
+        get() = _readmeState
+
+    sealed interface State {
+        object Loading : State
+        data class Error(val error: RepositoryError) : State
+
+        data class Loaded(
+            val githubRepo: RepoDetails,
+        ) : State
+    }
+
+    sealed interface ReadmeState {
+        object Loading : ReadmeState
+        object Empty : ReadmeState
+        data class Error(val error: RepositoryError) : ReadmeState
+        data class Loaded(val markdown: String) : ReadmeState {
+            fun markdownToString(): String {
+                return String(Base64.decode(markdown, Base64.DEFAULT), charset("UTF-8"))
+            }
+        }
+    }
+
+    private val fetchRepositoryExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        if (BuildConfig.DEBUG) Log.e(TAG, "Error: ", throwable)
+        if (throwable.message?.contains("hostname") == true) {
+            _state.postValue(State.Error(RepositoryError.NetworkError))
+
+        } else {
+            _state.postValue(State.Error(RepositoryError.Error(throwable.message.toString())))
+
+        }
+    }
+    private val fetchReadmeExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        if (BuildConfig.DEBUG) Log.e(TAG, "Error: ", throwable)
+        if (throwable.message?.contains("hostname") == true) {
+            _readmeState.postValue(ReadmeState.Error(RepositoryError.NetworkError))
+        } else {
+            _readmeState.postValue(ReadmeState.Error(RepositoryError.Error(throwable.message.toString())))
+        }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        fetchRepositoryDetailsJob?.cancel()
+        fetchRepositoryReadmeJob?.cancel()
+    }
+
+    fun fetchRepository(repositoryName: String) {
+        fetchRepositoryDetailsJob =
+            CoroutineScope(Dispatchers.IO + fetchRepositoryExceptionHandler).launch {
+                _state.postValue(State.Loading)
+                val response = repository.getRepository(repositoryName)
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        _state.postValue(State.Loaded(response.body()!!))
+                        if (response.body() is RepoDetails) response.body()?.let {
+                            fetchReadme(
+                                repositoryName = it.name,
+                                defaultBranch = it.defaultBranch,
+                                owner = it.owner.login
+                            )
+                        }
+                    } else {
+                        _state.postValue(
+                            State.Error(
+                                RepositoryError.Error(response.code().toString())
+                            )
+                        )
+                    }
+                }
+            }
+    }
+
+    fun fetchReadme(repositoryName: String, defaultBranch: String, owner: String) {
+        fetchRepositoryReadmeJob =
+            CoroutineScope(Dispatchers.IO + fetchReadmeExceptionHandler).launch {
+                _readmeState.postValue(ReadmeState.Loading)
+                val response = repository.getRepositoryReadme(
+                    ownerName = owner,
+                    repositoryName = repositoryName,
+                    branchName = defaultBranch,
+                )
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        _readmeState.postValue(ReadmeState.Loaded(response.body()!!.content))
+                    } else {
+                        if (response.code() == 404) {
+                            _readmeState.postValue(ReadmeState.Empty)
+                        } else {
+                            _readmeState.postValue(ReadmeState.Error(RepositoryError.Error(
+                                response.code().toString())))
+                        }
+                    }
+                }
+            }
+    }
+
+    fun logOut() = repository.logOut()
+
+}
