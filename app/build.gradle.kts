803a5afca5ba3a817bdab921ff02fb3b456c8663
Vladislav Kheynov <lcastory86@gmail.com>
2022-06-18 21:27:04 +0700

    build: v1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.kheynov.icerockgithubviewer"
         minSdk = 21
         targetSdk = 31
-        versionCode = 7
-        versionName = "1.2.6"
+        versionCode = 8
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
